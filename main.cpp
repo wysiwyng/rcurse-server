7ac38de51abcb7c8455dfccfcd8edaec20e8dde6
wysiwyng <wysiwyng@users.noreply.github.com>
2015-06-26 13:55:29 +0200

    first additions to main.cpp, added pugixml as submodule

--- main.cpp
+++ main.cpp
@@ -5,6 +5,21 @@
  *      Author: wysiwyng
  */
 
+#include <iostream>
+#include <sstream>
+#include <cstdlib>
 
+int main(int argc, char* argv[]) {
+	int port = 0;
+	std::string inp;
 
+	if(argc == 1) {
+		std::cout << "port: ";
+		std::cin >> port;
+	}
+	else if(argc == 2) {
+		port = atoi(argv[1]);
+	}
+	std::cout << "starting server on port " << port << std::endl;
 
+}
