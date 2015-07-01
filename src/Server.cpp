682ab71b15e890698e95c1e951c7c045a856b5a7
wysiwyng <wysiwyng@users.noreply.github.com>
2015-07-01 23:38:16 +0200

    added first parts of server functionality

--- src/Server.cpp
+++ src/Server.cpp
@@ -5,17 +5,50 @@
  *      Author: karsten
  */
 
+#include <cstdlib>
+#include <iostream>
+#include <functional>
+#include <thread>
 #include "Server.h"
 
 namespace rcurse_server {
 
-Server::Server() {
+Server::Server(unsigned short _port) :
+port(_port)
+{
 	// TODO Auto-generated constructor stub
 
 }
 
 Server::~Server() {
 	// TODO Auto-generated destructor stub
+}
+
+void Server::run() {
+	try {
+		listener = std::thread(std::bind(&Server::listen_thread, this));
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void Server::listen_thread() {
+	try {
+		using asio::ip::tcp;
+		tcp::acceptor a(io, tcp::endpoint(tcp::v4(), port));
+		while(1) {
+			std::shared_ptr<tcp::socket> sock(new tcp::socket(io));
+			a.accept(*sock);
+			std::shared_ptr<std::thread> receiver(new std::thread(std::bind(&Server::receive_thread, this, sock)));
+			receivers.push_back(receiver);
+		}
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+}
+
+void Server::receive_thread(std::shared_ptr<asio::ip::tcp::socket> sock) {
+
 }
 
 } /* namespace rcurse_server */
--- src/Server.h
+++ src/Server.h
@@ -8,12 +8,25 @@
 #ifndef SERVER_H_
 #define SERVER_H_
 
+#include <vector>
+#include "asio.hpp"
+
 namespace rcurse_server {
 
 class Server {
+private:
+	void listen_thread();
+	void receive_thread(std::shared_ptr<asio::ip::tcp::socket> sock);
+
+	std::vector<std::shared_ptr<std::thread>> receivers;
+	std::thread listener;
+	asio::io_service io;
+	unsigned short port;
 public:
-	Server();
+	Server(unsigned short _port);
 	virtual ~Server();
+
+	void run();
 };
 
 } /* namespace rcurse_server */
